fb882684a18fdc21f1a8dede7f870b07aebca45a
Dinu Berinde <dinu2193@gmail.com>
2021-07-16 00:11:59 +0200

    implementing the Search component

---
+++ src/components/Search.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="margin-top-page">
+    <b-card
+        title="Hotexplorer"
+    >
+      <b-card-text>
+        Navigate through blockchain objects
+      </b-card-text>
+
+      <b-card-body>
+
+        <b-form @submit.prevent="onSearchClick" inline style="display: block !important;">
+          <b-form-input
+              id="input-object-address"
+              type="text"
+              v-model="objectAddress"
+              placeholder="Enter object's address"
+              :state="isFormValid"
+              style="width: 60%"
+              required
+          ></b-form-input>
+
+          <b-button variant="primary" type="submit">Search</b-button>
+        </b-form>
+
+      </b-card-body>
+    </b-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Search",
+  data() {
+    return {
+      objectAddress: null,
+    }
+  },
+  methods: {
+    onSearchClick() {
+      this.$emit('onSearch', this.objectAddress)
+    }
+  },
+  computed: {
+    isFormValid() {
+      return this.objectAddress !== null && this.objectAddress === '' ? false : null
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
