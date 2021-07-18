33edcff3033e5c2f1f6bb8f8fc6221ab51f3a725
Dinu Berinde <dinu2193@gmail.com>
2021-07-18 17:45:30 +0200

    adding storage reference values

--- src/components/Explorer.vue
+++ src/components/Explorer.vue
@@ -24,16 +24,18 @@
         </div>
 
         <div class="accordion" role="tablist">
+
+          <b-card-text class="header-updates">Values fields</b-card-text>
           <div
-              v-for="(update, index) in state.updates"
+              v-for="(update, index) in getStorageValues"
               :key="index"
           >
             <b-card no-body class="mb-1" v-if="update.field && update.field.name">
               <b-card-header header-tag="header" class="p-1" role="tab">
-                <b-button block v-b-toggle="'collapsed-' + index" variant="light" class="highlighted">{{update.field.name}}</b-button>
+                <b-button block v-b-toggle="'collapsed-value-' + index" variant="light" class="highlighted">{{update.field.name}}</b-button>
               </b-card-header>
 
-              <b-collapse :id="'collapsed-' + index" accordion="my-accordion" role="tabpanel" v-if="update.field && update.field.type">
+              <b-collapse :id="'collapsed-value-' + index" accordion="my-accordion" role="tabpanel" v-if="update.field && update.field.type">
                 <b-card-body class="text-center">
 
                   <div class="row">
@@ -77,6 +79,62 @@
 
             </b-card>
           </div>
+
+          <b-card-text class="header-updates">Storage references fields</b-card-text>
+          <div
+              v-for="(update, index) in getStorageReferences"
+              :key="index + getStorageValues.length"
+          >
+            <b-card no-body class="mb-1" v-if="update.field && update.field.name">
+              <b-card-header header-tag="header" class="p-1" role="tab">
+                <b-button block v-b-toggle="'collapsed-reference-' + index + getStorageValues.length" variant="light" class="highlighted">{{update.field.name}}</b-button>
+              </b-card-header>
+
+              <b-collapse :id="'collapsed-reference-' + index + getStorageValues.length" accordion="my-accordion" role="tabpanel" v-if="update.field && update.field.type">
+                <b-card-body class="text-center">
+
+                  <div class="row">
+                    <div class="col-sm-6 col-xs-12">
+
+                      <div class="d-block text-left highlighted">
+                        Field
+                      </div>
+
+                      <div class="d-block text-left">
+                        Name <code>{{update.field.name}}</code>
+                      </div>
+
+                      <div class="d-block text-left">
+                        Type <code>{{update.field.type}}</code>
+                      </div>
+
+                      <div class="d-block text-left">
+                        Defining class <code>{{update.field.definingClass}}</code>
+                      </div>
+                    </div>
+
+                    <div class="col-sm-6 col-xs-12">
+                      <div class="d-block text-left highlighted">
+                        Value
+                      </div>
+
+                      <div class="d-block text-left">
+                        Address <code>{{update.value.reference.transaction.hash}}#{{parseInt(update.value.reference.progressive).toString(16)}}</code>
+                      </div>
+
+                      <div class="d-block text-left">
+                        Type <code>{{update.value.type}}</code>
+                      </div>
+                    </div>
+                  </div>
+
+
+                </b-card-body>
+              </b-collapse>
+
+            </b-card>
+          </div>
+
         </div>
       </b-card-body>
     </b-card>
@@ -101,6 +159,12 @@ export default {
     getRootObject() {
       const classNames = this.state.updates.filter(update => update.className !== undefined && update.className !== null)
       return classNames.length > 0 ? classNames[0] : null
+    },
+    getStorageValues() {
+      return this.state.updates.filter(update => update.value && update.value.type && update.value.type !== 'reference')
+    },
+    getStorageReferences() {
+      return this.state.updates.filter(update => update.value && update.value.type && update.value.type === 'reference')
     }
   }
 }
@@ -117,6 +181,13 @@ export default {
 
 .highlighted {
   font-weight: bold
+}
+
+.header-updates {
+  text-align: left !important;
+  color: rgba(0,0,0,.65) !important;
+  font-weight: bold;
+  margin-top: 1rem !important;
 }
 
 </style>
