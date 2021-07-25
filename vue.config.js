40b502d3bb29c8e7e12b89bd1532860ae6cafc5c
Dinu Berinde <dinu2193@gmail.com>
2021-07-25 17:33:38 +0200

    local proxy to blockchain

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,24 @@
 module.exports = {
     devServer: {
         // local blockchain node
-        proxy: 'http://localhost:8080/api',
+        proxy: {
+            '/get/*': {
+                target: 'http://localhost:8080',
+                changeOrigin: true,
+            },
+            '/run/*': {
+                target: 'http://localhost:8080',
+                changeOrigin: true,
+            },
+            '/add/*': {
+                target: 'http://localhost:8080',
+                changeOrigin: true,
+            },
+            '/post/*': {
+                target: 'http://localhost:8080',
+                changeOrigin: true,
+            }
+        },
         port: 4001
     }
 }
