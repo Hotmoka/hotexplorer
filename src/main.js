159b9b54e09db979059306ee272769ba17a9f24c
Dinu Berinde <dinu2193@gmail.com>
2021-07-31 15:20:37 +0200

    adding router

--- src/main.js
+++ src/main.js
@@ -2,12 +2,25 @@ import Vue from 'vue'
 import App from './App.vue'
 import PortalVue from 'portal-vue'
 import {BootstrapVue, IconsPlugin} from 'bootstrap-vue'
+import VueRouter from 'vue-router'
 
+Vue.use(VueRouter)
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
 Vue.use(PortalVue)
 Vue.config.productionTip = false
 
+
+const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    { path: '/', component: App },
+    { path: '/:address', component: App },
+    { path: '*', redirect: '/' }
+  ]
+})
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
